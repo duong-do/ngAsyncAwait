b6ad5021d7e91c59043a18017ca4300723d11662
Duong Do <duongd@aareon.nl>
2018-03-21 10:26:27 +0100

    Added async code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  countries: Array<any>;
+
+  constructor(private appService: AppService) {
+    this.doAsyncJobs().then(() => {
+      this.sleep(30000);
+      console.log('Finish!');
+    });
+    console.log('Not async');
+  }
+
+  async doAsyncJobs() {
+    await this.getCountries();
+    await this.getCountries2();
+    console.log('Countries are loaded 2 times.');
+  }
+
+  getCountries(): Promise<any> {
+    return this.appService.getCountries().toPromise().then((data) => {
+      this.countries = data;
+      this.countries.forEach(c => {
+        c.value = c.code;
+        c.label = c.name;
+      });
+      console.log('Countries are loading ...');
+      // this.delay(30000).then();
+      this.sleep(3000000000);
+    });
+  }
+
+  getCountries2(): Promise<any> {
+    return this.appService.getCountries().toPromise().then((data) => {
+      this.countries = data;
+      this.countries.forEach(c => {
+        c.value = c.code;
+        c.label = c.name;
+      });
+      console.log('Countries are loaded second time');
+    });
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms)).then();
+  }
+
+  sleep(teller) {
+    const start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > teller) {
+        break;
+      }
+    }
+  }
 }
